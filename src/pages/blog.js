18549bcb20be705184926ce0eda4a617a9743024
dakeobac <dakeobac@gmail.com>
2021-03-16 19:03:13 +0100

    making changes...

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const BlogPage = () => {
+    return (
+        <Layout>
+            Blog posts will show up here...
+        </Layout>
+    )
+}
+
+export default BlogPage
